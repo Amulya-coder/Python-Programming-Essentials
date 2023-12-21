436fbd565c94a4e8754fa6c097268870032136f0
Amulya Dixit <amulyadixit07@gmail.com>
2023-12-21 23:16:58 +0530

    list of characters into a string

---
+++ Python_C2_Assignment/Q6.py
@@ -0,0 +1,8 @@
+# Python program to convert a list of characters into a string.
+
+input=['a', 'b', 'c', 'd']
+
+# Using the join function to join the list a separating by ""
+res="".join(input)
+print(res)
+    
