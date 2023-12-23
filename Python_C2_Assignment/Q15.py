bccc3a9e5660b3c3f4f21d73faaa1a59e761eac7
Amulya Dixit <amulyadixit07@gmail.com>
2023-12-23 22:25:12 +0530

    string to tuple

---
+++ Python_C2_Assignment/Q15.py
@@ -0,0 +1,4 @@
+# Python program to print a tuple with string formatting.
+
+t=(100, 200, 300)
+print(f'This is tuple {t}')
---
+++ Python_C2_Assignment/Q16.py
@@ -0,0 +1,4 @@
+# Python program to convert a given string to a 
+
+s="Shailja"
+print(tuple(s))
