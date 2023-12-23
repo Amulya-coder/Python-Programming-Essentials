5319339c7e502e8958661fc41ce0fc8537965937
Amulya Dixit <amulyadixit07@gmail.com>
2023-12-23 22:45:47 +0530

    List of unique strings using set

---
+++ Python_C2_Assignment/Q22.py
@@ -0,0 +1,6 @@
+# Python program to remove all duplicates from a given list of strings
+# and return a list of unique strings.
+
+l1= ['Python', 'Exercises', 'Practice', 'Solution', 'Exercises']
+l1=set(l1)
+print(list(l1))
