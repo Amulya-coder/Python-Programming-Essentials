235cbc4a7131f364c433231a146dacaa89e1d1f7
Amulya Dixit <amulyadixit07@gmail.com>
2023-12-28 23:05:28 +0530

    replace all occurences with given character

---
+++ Python_C3_Assignment/Q7.py
@@ -0,0 +1,8 @@
+# Python program that takes a string as input and replaces all
+# occurrences of a given character with another character.
+
+s='We study at GrowDataSkills'
+replace=input()
+character=input()
+x=s.replace(replace,character)
+print(x)
