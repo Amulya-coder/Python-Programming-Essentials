2193460a1dd4dab7a9c7628e795f360753c7ef18
Amulya Dixit <amulyadixit07@gmail.com>
2023-12-30 00:19:08 +0530

    Occurence of element

---
+++ Python_C3_Assignment/Q15.py
@@ -0,0 +1,9 @@
+'''
+Python function that takes a list and an element as input and returns
+the number of occurrences of that element in the list.
+'''
+
+input_list = [ 1,2,3,4,2,2,3,4,5,9,2,6]
+n=2
+x=input_list.count(n)
+print('Occurence: ',x)
