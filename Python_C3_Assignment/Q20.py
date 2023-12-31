87ed408cea6f7f1d82c3b0552b6f3ace662580b5
Amulya Dixit <amulyadixit07@gmail.com>
2023-12-31 16:21:22 +0530

    Calculate area and perimeter using classes

---
+++ Python_C3_Assignment/Q20.py
@@ -0,0 +1,16 @@
+'''
+Python program to create a class representing a Circle. Include
+methods to calculate its area and perimeter.
+'''
+
+import math
+
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
+        
+    def calculate_area(self):
+        return math.pi*self.radius**2
+        
+    def calculate_perimeter(self):
+        return 2*math.pi*self.radius
