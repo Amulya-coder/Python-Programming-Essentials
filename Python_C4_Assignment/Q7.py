68953c62545d9fca1454ba302124805c57f4b197
Amulya Dixit <amulyadixit07@gmail.com>
2024-01-04 23:35:07 +0530

    Diagonal of numpy

---
+++ Python_C4_Assignment/Q7.py
@@ -0,0 +1,7 @@
+# Given a 2D NumPy array:
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# Get the diagonal elements of the array
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+a=np.diagonal(arr)
+print(a)
