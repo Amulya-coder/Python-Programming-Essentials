80f6a5b5e48dffee2f731ca6b2be3816fa986b7c
Amulya Dixit <amulyadixit07@gmail.com>
2024-01-06 00:03:50 +0530

    Sort the dataframe based on the age column in ascending order

--- Python_C4_Assignment/Q18.py
+++ Python_C4_Assignment/Q18.py
@@ -1,4 +1,4 @@
 # Create a new DataFrame named 'df_filtered' from 'df', containing only rows where 
 # the 'Age' column is greater than 25
 
-sorted_df = df.sort_values(by='Age', ascending=True)
+df_filtered=df[df['Age']>25]
---
+++ Python_C4_Assignment/Q19.py
@@ -0,0 +1,4 @@
+# Given a Pandas DataFrame 'df', sort the DataFrame based on the 'Age' column in
+# ascending order.
+
+sorted_df = df.sort_values(by='Age', ascending=True)
