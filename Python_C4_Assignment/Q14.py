7654f8fc68338a487f5bb9f37f7d6d3379a2ed4a
Amulya Dixit <amulyadixit07@gmail.com>
2024-01-05 23:51:01 +0530

    converting dictionary into pandas dataframe

---
+++ Python_C4_Assignment/Q14.py
@@ -0,0 +1,7 @@
+# Pandas DataFrame from the following dictionary, where the keys represent
+# columns and the values represent the data
+
+data = { 'Name': ['Alice', 'Bob', 'Charlie', 'David'], 'Age': [25, 30, 22, 28], 'City': ['New York',
+'London', 'Paris', 'Tokyo'] }
+
+df = pd.DataFrame(data)
