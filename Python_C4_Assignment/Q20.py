6d34d0efee48e9da46eac3d03d5bf5356f85fc07
Amulya Dixit <amulyadixit07@gmail.com>
2024-01-06 00:05:01 +0530

    mean and median of Salary column

---
+++ Python_C4_Assignment/Q20.py
@@ -0,0 +1,5 @@
+# Calculate the mean and median of the 'Salary' column in the DataFrame 'df'.
+
+mean_salary = df['Salary'].mean()
+median_salary = df['Salary'].median()
+print(mean_salary, median_salary)
